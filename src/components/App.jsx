b6261a48abef43cc571ec3bee6dab71146fb3f5f
Olena Bieletska <belshel12@gmail.com>
2024-09-10 21:20:46 +0300

    add Local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,33 @@ import initialContacts from "../contacts.json";
 import Filter from "./Filter/Filter";
 import ContactList from "./ContactList/ContactList";
 import ContactForm from "./ContactForm/ContactForm";
+import toast, { Toaster } from "react-hot-toast";
 
 class App extends Component {
   state = {
-    contacts: initialContacts,
+    contacts: [],
     filter: "",
   };
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(localContacts);
+
+    !parsedContacts
+      ? this.setState({ contacts: initialContacts })
+      : this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  }
 
   createContact = (newUser) => {
     this.state.contacts.some(
       ({ name }) =>
         name.toLocaleLowerCase() === newUser.name.toLocaleLowerCase()
     )
-      ? alert(`${newUser.name} is already in contacts.`)
+      ? toast.error(`${newUser.name} is already in contacts.`)
       : this.setState((prev) => ({
           contacts: [...prev.contacts, newUser],
         }));
@@ -25,7 +39,7 @@ class App extends Component {
     this.setState({ filter: data });
   };
 
-  handleDeleteContact = (id) => {
+  handleDelete = (id) => {
     this.setState((prev) => ({
       contacts: prev.contacts.filter((contact) => {
         return id !== contact.id;
@@ -33,21 +47,26 @@ class App extends Component {
     }));
   };
 
-  render() {
+  handleFilter = () => {
     const { contacts, filter } = this.state;
     const filterContacts = contacts.filter((el) =>
       el.name.toLowerCase().includes(filter.toLowerCase())
     );
+    return filterContacts;
+  };
 
+  render() {
+    const { contacts, filter } = this.state;
     return (
       <>
+        <Toaster toastOptions={{ duration: 1500 }} />
         <h1>Phonebook</h1>
         <ContactForm getData={this.createContact} />
         <h2>Contacts</h2>
         <Filter getFilter={this.getFilterData} />
         <ContactList
-          contacts={filter ? filterContacts : contacts}
-          onDelete={this.handleDeleteContact}
+          contacts={filter ? this.handleFilter() : contacts}
+          onDelete={this.handleDelete}
         />
       </>
     );
